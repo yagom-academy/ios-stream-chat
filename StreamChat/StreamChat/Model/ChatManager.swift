9d26cb7ead3c78450643461754a2be5e8dc95c2e
innieminnie <rkddlsl1001@gmail.com>
2021-04-20 16:11:07 +0900

    feat: ChatManager 구조체 생성 및 connectSocket() 구현

---
+++ StreamChat/StreamChat/Model/ChatManager.swift
@@ -0,0 +1,25 @@
+
+import Foundation
+
+struct ChatManager {
+    private var inputStream: InputStream?
+    private var outputStream: OutputStream?
+    
+    func connectSocket() {
+        Stream.getStreamsToHost(withName: Host.address, port: Host.port, inputStream: &inputStream, outputStream: &outputStream)
+        
+        guard let inputStream = self.inputStream,
+              let outputStream = self.outputStream else {
+            return
+        }
+        
+        let myRunLoop = RunLoop.current
+        inputStream.schedule(in: myRunLoop, forMode: .default)
+        inputStream.delegate = self
+        
+        inputStream.open()
+        outputStream.open()
+        
+        print("current ip : \(Host.address)")
+    }
+}
