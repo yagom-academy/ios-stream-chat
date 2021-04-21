732cbf32ee5604b54231819a308b956606b6e875
innieminnie <rkddlsl1001@gmail.com>
2021-04-21 11:07:43 +0900

    feat: 유저의 채팅 접속 메소드 추가

--- StreamChat/StreamChat/Model/ChatManager.swift
+++ StreamChat/StreamChat/Model/ChatManager.swift
@@ -1,9 +1,9 @@
 
 import Foundation
 
-class ChatManager: NSObject, StreamDelegate {    
-   private var inputStream: InputStream?
-   private var outputStream: OutputStream?
+class ChatManager: NSObject {
+    var inputStream: InputStream?
+    var outputStream: OutputStream?
     
     func connectSocket() {
         Stream.getStreamsToHost(withName: Host.address, port: Host.port, inputStream: &inputStream, outputStream: &outputStream)
@@ -15,9 +15,45 @@ class ChatManager: NSObject, StreamDelegate {
         
         let runLoop = RunLoop.current
         inputStream.schedule(in: runLoop, forMode: .default)
+        outputStream.schedule(in: runLoop, forMode: .default)
+        
         inputStream.delegate = self
+        outputStream.delegate = self
         
         inputStream.open()
         outputStream.open()
+    }
+    
+    func join(with client: Client) {
+        let joinMessage = CommunicationMessage.join(client.nickname).description
+        guard let data = joinMessage.data(using: .utf8) else {
+            return
+        }
+        
+        data.withUnsafeBytes {
+            guard let pointer = $0.baseAddress?.assumingMemoryBound(to: UInt8.self) else {
+                print("Error joining chat")
+                return
+            }
+            
+            if let outputStream = self.outputStream {
+                outputStream.write(pointer, maxLength: data.count)
+            }
+        }
+    }
+}
+extension ChatManager: StreamDelegate {
+    func stream(_ aStream: Stream, handle eventCode: Stream.Event) {
+        switch eventCode {
+        case .hasBytesAvailable:
+            let iStream  =  aStream as! InputStream
+            let bufferSize = 1024
+            var buffer = [UInt8](repeating: 0, count: bufferSize)
+            iStream.read(&buffer, maxLength: bufferSize)
+            let msg = String(bytes: buffer, encoding: .utf8)
+            print(msg)
+        default:
+            print("some other event...")
+        }
     }
 }
