1e652eb30edbe34983d3faae0b2c0a2a81a09e44
taetae <taetae.dev@gmail.com>
2021-04-26 16:44:49 +0900

    add: Host 정보를 담는 구조체 추가

---
+++ StreamChat/StreamChat/Model/ChatRoom.swift
@@ -0,0 +1,8 @@
+//
+//  ChatRoom.swift
+//  StreamChat
+//
+//  Created by 김태형 on 2021/04/26.
+//
+
+import Foundation
---
+++ StreamChat/StreamChat/Model/Host.swift
@@ -0,0 +1,13 @@
+//
+//  Host.swift
+//  StreamChat
+//
+//  Created by 김태형 on 2021/04/23.
+//
+
+import Foundation
+
+struct Host {
+    static let address = "stream-ios.yagom-academy.kr"
+    static let port = 7748
+}
