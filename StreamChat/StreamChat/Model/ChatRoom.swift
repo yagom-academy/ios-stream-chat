af4110b6218e556b6185137a6bd10bf257ed3759
taetae <taetae.dev@gmail.com>
2021-04-26 16:47:59 +0900

    add: 연결에 필요한 ChatRoom클래스 추가 및 connect() 구현

--- StreamChat/StreamChat/Model/ChatRoom.swift
+++ StreamChat/StreamChat/Model/ChatRoom.swift
@@ -2,7 +2,40 @@
 //  ChatRoom.swift
 //  StreamChat
 //
-//  Created by 김태형 on 2021/04/26.
+//  Created by 김태형 on 2021/04/20.
 //
 
 import Foundation
+
+class ChatRoom: NSObject, StreamDelegate {
+    
+    private var inputStream: InputStream!
+    private var outputStream: OutputStream!
+    private let maxLength = 300
+    private var username = ""
+    
+    
+    func connect() {
+        Stream.getStreamsToHost(withName: Host.address, port : Host.port, inputStream: &inputStream, outputStream: &outputStream)
+        
+        guard let inputStream = self.inputStream,
+              let outputStream = self.outputStream else {
+            return
+        }
+        
+        inputStream.delegate = self
+        outputStream.delegate = self
+        
+        inputStream.schedule(in: .main, forMode: RunLoop.Mode.default)
+        outputStream.schedule(in: .main, forMode: RunLoop.Mode.default)
+        
+        inputStream.open()
+        outputStream.open()
+    }
+    
+   
+}
+
+
+
+
