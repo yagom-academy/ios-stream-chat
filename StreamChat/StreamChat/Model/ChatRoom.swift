4596d0f72183edd7a53384b55685aacf1b934950
taetae <taetae.dev@gmail.com>
2021-04-26 16:50:33 +0900

    feat: 입력 이벤트 발생시 메시지 출력, 메시지 읽기기능

--- StreamChat/StreamChat/Model/ChatRoom.swift
+++ StreamChat/StreamChat/Model/ChatRoom.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-class ChatRoom: NSObject, StreamDelegate {
+class ChatRoom: NSObject {
     
     private var inputStream: InputStream!
     private var outputStream: OutputStream!
@@ -65,6 +65,32 @@ class ChatRoom: NSObject, StreamDelegate {
         }
 }
 
-
+extension ChatRoom: StreamDelegate {
+    func stream(_ stream: Stream, handle eventCode: Stream.Event) {
+        switch eventCode {
+        case Stream.Event.errorOccurred:
+            print("ErrorOccurred")
+        case Stream.Event.openCompleted:
+            print("OpenCompleted")
+        case Stream.Event.hasBytesAvailable:
+            print("HasBytesAvailable")
+            readAvailableBytes(stream: stream)
+        case Stream.Event.endEncountered:
+            print("EndEncountered")
+        default:
+            print("unknown event")
+        }
+    }
+    
+    private func readAvailableBytes(stream: Stream) {
+        let buffer = UnsafeMutablePointer<UInt8>.allocate(capacity: maxLength)
+        let readByte = inputStream.read(buffer, maxLength: maxLength)
+        
+        guard let message = String(bytesNoCopy: buffer, length: readByte, encoding: .utf8, freeWhenDone: true) else {
+            return
+        }
+        print(message)
+    }
+}
 
 
