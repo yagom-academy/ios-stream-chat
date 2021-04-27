26c46fc692f33d67d1262eb51afa8da226a41d87
odong-Tree <ss12357@naver.com>
2021-04-27 11:06:16 +0900

    feat: ChatRoom타입에 받은 메세지를 다룰 수 있도록 StreamDelegate 채택 및 코드 추가 (#1)

--- StreamChat/StreamChat/ChatRoom.swift
+++ StreamChat/StreamChat/ChatRoom.swift
@@ -24,6 +24,7 @@ class ChatRoom: NSObject {
         inputStream = readStream!.takeRetainedValue()
         outputStream = writeStream!.takeRetainedValue()
         
+        inputStream.delegate = self
         inputStream.schedule(in: .current, forMode: .common)
         outputStream.schedule(in: .current, forMode: .common)
         
@@ -43,5 +44,49 @@ class ChatRoom: NSObject {
             
             outputStream.write(pointer, maxLength: data.count)
         }
+    }
+}
+
+extension ChatRoom: StreamDelegate {
+    func stream(_ aStream: Stream, handle eventCode: Stream.Event) {
+        switch eventCode {
+        case .hasBytesAvailable:
+            print("new message received")
+            readAvailableBytes(stream: aStream as! InputStream)
+        case .endEncountered:
+            print("new message received")
+        case .errorOccurred:
+            print("error occurred")
+        case .hasSpaceAvailable:
+            print("has space available")
+        default:
+            print("some other event...")
+        }
+    }
+    
+    private func readAvailableBytes(stream: InputStream) {
+        let buffer = UnsafeMutablePointer<UInt8>.allocate(capacity: maxReadLength)
+        
+        while stream.hasBytesAvailable {
+            let numberOfBytesRead = inputStream.read(buffer, maxLength: maxReadLength)
+            
+            if numberOfBytesRead < 0, let error = stream.streamError {
+                print(error)
+                break
+            }
+        }
+    }
+   
+    private func processedMessageString(buffer: UnsafeMutablePointer<UInt8>, length: Int) -> Message? {
+      guard let stringArray = String(bytesNoCopy: buffer, length: length, encoding: .utf8, freeWhenDone: true)?.components(separatedBy: ":"),
+        let name = stringArray.first,
+        let message = stringArray.last
+        else {
+          return nil
+      }
+        
+      let messageSender: MessageSender =
+        (name == self.username) ? .ourself : .someoneElse
+      return Message(message: message, messageSender: messageSender, username: name)
     }
 }
