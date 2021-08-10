b72709e7f6209dae7c34d1f0d9320dc802414793
ryan-son <ryan.son1002@gmail.com>
2021-08-11 04:00:31 +0900

    feat: LoginViewController 생성 및 entryViewController 변경
    Fixes: #4

---
+++ StreamChat/StreamChat/Sources/Views/LoginViewController.swift
@@ -0,0 +1,15 @@
+//
+//  LoginViewController.swift
+//  StreamChat
+//
+//  Created by Ryan-Son on 2021/08/11.
+//
+
+import UIKit
+
+final class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
