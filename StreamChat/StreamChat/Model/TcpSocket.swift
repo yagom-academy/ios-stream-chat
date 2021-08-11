d0057c016797720b50e0620f5d3455ef96999608
kangkyung <rudgns0508@naver.com>
2021-08-11 22:54:34 +0900

    [#3]Feat: TCP Socket 모델 생성
    - [x] connect 구현
    - [x] send 구현
    - [x] receive 구현
    - [x] disconnect 구현

---
+++ StreamChat/StreamChat/Model/TcpSocket.swift
@@ -0,0 +1,44 @@
+//
+//  TcpSocket.swift
+//  StreamChat
+//
+//  Created by 강경 on 2021/08/09.
+//
+
+import Foundation
+
+class TcpSocket: NSObject {
+    
+    var inputStream: InputStream?
+    var outputStream: OutputStream?
+    
+    func connect(host: String, port: Int) {
+        Stream.getStreamsToHost(withName: host, port: port, inputStream: &inputStream,
+                                outputStream: &outputStream)
+        inputStream?.open()
+        outputStream?.open()
+    }
+    func send(data: String) {
+        outputStream?.write(data, maxLength: data.count)
+    }
+    func receive(totalSizeOfBuffer: Int) throws -> Data {
+        var buffer = [UInt8](repeating: 0, count: totalSizeOfBuffer)
+        guard let bufferOfReaded = inputStream?.read(&buffer, maxLength: totalSizeOfBuffer) else {
+            throw TcpError.failedToReadInputStream
+        }
+
+        let differenceOfBufferBytes = totalSizeOfBuffer - bufferOfReaded
+        if differenceOfBufferBytes > 0 {
+            let countToDrop = differenceOfBufferBytes
+            let customizedBuffer = buffer.dropLast(countToDrop)
+            
+            return Data(customizedBuffer)
+        } else {
+            throw TcpError.noDataReceived
+        }
+    }
+    func disconnect() {
+        inputStream?.close()
+        outputStream?.close()
+    }
+}
