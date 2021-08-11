18667f80a4742650aa87d1a614296dab67de9c9d
kangkyung <rudgns0508@naver.com>
2021-08-12 00:26:23 +0900

    [#8]Feat: 보내는 메시지 글자수 제한 테스트 구현

--- StreamChat/StreamChatTests/StreamChatTests.swift
+++ StreamChat/StreamChatTests/StreamChatTests.swift
@@ -38,7 +38,29 @@ final class StreamChatTests: XCTestCase {
         
         XCTAssertEqual(messageToSend, receivedMessage)
     }
-    // TODO: - 보내는 메시지 글자수 제한 테스트 구현 -> server에 의존하지 않게 만들면 되나..?
+    
+    func test_보내는_메시지_글자수_제한() throws {
+        let sixtyCharacterString = "012345678901234567890123456789012345678901234567890123456789"
+        let threeHundredCharacterString = """
+            \(sixtyCharacterString)\(sixtyCharacterString)\(sixtyCharacterString)
+            \(sixtyCharacterString)\(sixtyCharacterString)
+            """
+        let messageToSend = threeHundredCharacterString
+        let chatting = Chatting(userName: userName, host: host, port: port)
+        chatting.enterTheChatRoom()
+        
+        do {
+            try chatting.send(message: messageToSend)
+        } catch {
+            let stringOfError: String = "\(error)"
+            XCTAssertEqual(stringOfError, ChattingError.sendingMessagesIsLimitedTo300.description)
+        }
+        
+        chatting.leaveTheChatRoom()
+        chatting.disconnect()
+    }
+    
+    // TODO: - 보내는 메시지 글자수 제한 테스트 구현
     // MARK: - 채팅참가 알림 및 수신
     func test_채팅참가_알림_및_수신() throws {
         let chatParticipationNotification = "\(actName) has joined"
