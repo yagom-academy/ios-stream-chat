fda81ee92577f6d6354012a609d77fb4a219fb2d
kane-young <lyw2100@naver.com>
2021-08-13 20:05:37 +0900

    feat: 메시지 receive 로직 확인용 구현

--- StreamChat/StreamChat/ViewController/ChatHomeViewController.swift
+++ StreamChat/StreamChat/ViewController/ChatHomeViewController.swift
@@ -8,7 +8,26 @@
 import UIKit
 
 class ChatHomeViewController: UIViewController {
+    let chatRoomManager = ChatRoomManager(host: ChatRoom.host, port: ChatRoom.port)
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        chatRoomManager.join(userName: "kane")
+        chatRoomManager.delegate = self
+    }
+
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        chatRoomManager.disconnect()
+    }
+}
+
+extension ChatHomeViewController: ChatRoomManagerDelegate {
+    func receive(_ chatDataFormat: Message) {
+        print(chatDataFormat)
+    }
+
+    func handleError(_ error: Error) {
+        print(error)
     }
 }
