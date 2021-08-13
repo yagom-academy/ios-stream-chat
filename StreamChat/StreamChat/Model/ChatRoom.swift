079b2f60c3feca92a3474f5b375b7a71bc43ba22
Inwoo Hwang <dlsdn1207@yonsei.ac.kr>
2021-08-13 14:14:26 +0900

    feat: ChatRoom 초기 구현
    - inputStream, outputStream구현
      - setUpNetwork()에서 streamTask를 활용하여 inputStream과 outputStream
        capture
    - URLSessionStreamDelegate의 urlSession메서드 작성
      - capture된 inputStream과 outputStream의 delegate 위임작업 및
        mainloop에 스케줄작업 후 open
    - joinChat() 메서드 구현
      - 유저가 입장할 때 호출될 메서드. 스트림 데이터에 맞게 데이터 구현 후
        outputStream의 write메서드를 통해 해당 데이터를 화면에 표출할 수 있게
       설정
    - readAvailableBytes() 초기 구현
    - StreamDelegate
      - streamEvent에 다양한 작업을 처리하 ㄹ수 있게 switch문을 활용하여 케이스 구별

---
+++ StreamChat/StreamChat/Model/ChatRoom.swift
@@ -0,0 +1,83 @@
+//
+//  ChatRoom.swift
+//  StreamChat
+//
+//  Created by James on 2021/08/12.
+//
+
+import UIKit
+
+final class ChatRoom: NSObject {
+    var urlSession: URLSession
+    var streamTask: URLSessionStreamTask?
+    var inputStream: InputStream?
+    var outputStream: OutputStream?
+    let maxReadLength = 2400
+    init(urlSession: URLSession = .shared) {
+        self.urlSession = urlSession
+        }
+    var username = ""
+    private func setUpNetwork() {
+        let configuration = URLSessionConfiguration.default
+        configuration.requestCachePolicy = .reloadIgnoringLocalCacheData
+        urlSession = URLSession(configuration: configuration, delegate: self, delegateQueue: .main)
+        streamTask = urlSession.streamTask(withHostName: StreamInformation.host, port: StreamInformation.portNumber)
+        streamTask?.startSecureConnection()
+        streamTask?.resume()
+        streamTask?.captureStreams()
+    }
+    func joinChat(username: String) {
+        guard let data = "USR_NAME::\(username)::END".data(using: .utf8) else {
+            return
+        }
+        data.withUnsafeBytes { unsafeRawBufferPointer in
+            guard let pointer = unsafeRawBufferPointer.baseAddress?.assumingMemoryBound(to: UInt8.self) else {
+                return
+
+            }
+            outputStream?.write(pointer, maxLength: data.count)
+        }
+    }
+    private func readAvailableBytes(stream: InputStream) {
+        let buffer = UnsafeMutablePointer<UInt8>.allocate(capacity: maxReadLength)
+        while stream.hasBytesAvailable {
+            let numberOfBytesRead = stream.read(buffer, maxLength: maxReadLength)
+            if numberOfBytesRead < 0 {
+                if let error = stream.streamError {
+                    NSLog(error.localizedDescription)
+                    break
+                }
+            }
+        }
+    }
+}
+extension ChatRoom: StreamDelegate {
+    func stream(_ aStream: Stream, handle eventCode: Stream.Event) {
+      switch eventCode {
+      case .hasBytesAvailable:
+        print("new message received")
+      case .endEncountered:
+        print("new message received")
+      case .errorOccurred:
+        print("error occurred")
+      case .hasSpaceAvailable:
+        print("has space available")
+      default:
+        print("some other event...")
+      }
+}
+}
+
+extension ChatRoom: URLSessionStreamDelegate {
+    func urlSession(_ session: URLSession, streamTask: URLSessionStreamTask, didBecome inputStream: InputStream, outputStream: OutputStream) {
+
+        self.inputStream = inputStream
+        self.inputStream?.delegate = self
+        self.outputStream = outputStream
+        self.outputStream?.delegate = self
+        self.inputStream?.schedule(in: .main, forMode: .default)
+        self.outputStream?.schedule(in: .main, forMode: .default)
+        self.inputStream?.open()
+        self.outputStream?.open()
+    }
+}
