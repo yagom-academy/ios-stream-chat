29182c5de1b5ee2294e6876e72a07cc4d54fef4a
Inwoo Hwang <dlsdn1207@yonsei.ac.kr>
2021-08-13 17:58:26 +0900

    feat: StreamError 모델 생성

---
+++ StreamChat/StreamChat/Model/StreamError.swift
@@ -0,0 +1,13 @@
+//
+//  StreamError.swift
+//  StreamChat
+//
+//  Created by 황인우 on 2021/08/13.
+//
+
+import Foundation
+
+enum StreamError: Error {
+    case connectionError
+    case unknownError
+}
