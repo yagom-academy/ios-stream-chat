474ce2bded53c64f59eade774e9cd6de8744ff0f
Inwoo Hwang <dlsdn1207@yonsei.ac.kr>
2021-08-14 17:50:16 +0900

    test: [trouble]chatRoom의 joinChat()메서드 테스트
    실패: Asynchronouswait failed. mock_streamtask 이니셜라이저 문제인 것 발견

--- StreamChat/StreamChatTests/StreamChatTests.swift
+++ StreamChat/StreamChatTests/StreamChatTests.swift
@@ -9,14 +9,46 @@ import XCTest
 
 class StreamChatTests: XCTestCase {
     var sut_chatroom: ChatRoom!
+    var mock_urlsession: URLSessionProtocol!
+    var mock_streamtask: MockStreamTask!
+    
     override func setUpWithError() throws {
+        
+        // given
+        let configuration = URLSessionConfiguration.default
+        configuration.requestCachePolicy = .reloadIgnoringCacheData
+        mock_urlsession = MockURLSession(configuration: configuration, delegate: nil, delegateQueue: nil)
+        sut_chatroom = ChatRoom(urlSession: mock_urlsession)
+        
     }
 
     override func tearDownWithError() throws {
+        mock_urlsession = nil
     }
-    func test_join_chat() {
-        sut_chatroom = ChatRoom()
-        sut_chatroom.setUpNetwork()
-        sut_chatroom.joinChat(username: "James")
+    
+    func test_sut_chatroom_에서_joinChat메서드_호출시_streamtask가_정상적으로_writeData를_할수있는지_체크() {
+        
+        // given
+        let expectation = XCTestExpectation()
+        let name = "James"
+        mock_streamtask = MockStreamTask()
+        
+        // when
+        mock_streamtask.resultHandler = {
+            let expectedString = String(data: self.mock_streamtask.dataList.first!, encoding: .utf8)!
+            
+            // then
+            // verify that mock streamtask was able to write data and add it to expected array
+            XCTAssertEqual(self.mock_streamtask.dataList.count, 1)
+            
+            // verify that the written data is an expected data
+            XCTAssertEqual(expectedString, "James123")
+            
+            // verify that write method was called only once
+            XCTAssertEqual(self.mock_streamtask.writeCounter, 1)
+            expectation.fulfill()
+        }
+        sut_chatroom.joinChat(username: name)
+        wait(for: [expectation], timeout: 3)
     }
 }
