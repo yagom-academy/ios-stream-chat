2371da8b50a1270f4cbf361ec8a462bb25e1310e
Inwoo Hwang <dlsdn1207@yonsei.ac.kr>
2021-08-14 17:46:29 +0900

    test: MockURLSession 구현

---
+++ StreamChat/StreamChatTests/MockURLSession.swift
@@ -0,0 +1,35 @@
+//
+//  MockURLSession.swift
+//  StreamChatTests
+//
+//  Created by James on 2021/08/13.
+//
+@testable import StreamChat
+import Foundation
+
+final class MockURLSession: URLSessionProtocol {
+    
+    var configuration: URLSessionConfiguration
+    var delegate: URLSessionDelegate?
+    var delegateQueue: OperationQueue?
+    var didStreamConnectionFail = false
+    var mockStreamTask: MockStreamTask?
+    
+    init(configuration: URLSessionConfiguration, delegate: URLSessionDelegate?, delegateQueue: OperationQueue?) {
+        self.configuration = configuration
+        self.delegate = delegate
+        self.delegateQueue = delegateQueue
+    }
+    
+    func invalidateAndCancel() {
+        mockStreamTask?.closeRead()
+        mockStreamTask?.closeWrite()
+    }
+    
+    func streamTask(withHostName hostname: String, port: Int) -> URLSessionStreamTask {
+        mockStreamTask = MockStreamTask()
+        return mockStreamTask!
+    }
+    
+    
+}
