bccf4c623f821bd5a52b404c8feb2237f31a970f
wody95 <kwu0626@gmail.com>
2021-08-18 12:34:00 +0900

    chore : SnapKit import 명시

--- StreamChat/StreamChat/MyMessageCell.swift
+++ StreamChat/StreamChat/MyMessageCell.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SnapKit
 
 final class MyMessageCell: UITableViewCell {
     static let reuseIfentifier = "myMessageCell"
--- StreamChat/StreamChat/NotificationCell.swift
+++ StreamChat/StreamChat/NotificationCell.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SnapKit
 
 final class NotificationCell: UITableViewCell {
     static let reuseIdentifier = "notificationCell"
