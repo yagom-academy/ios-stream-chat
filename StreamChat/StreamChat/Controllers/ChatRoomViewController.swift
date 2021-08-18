c55e768318e7f68292c5a7b83f38d3e576e37054
Inwoo Hwang <dlsdn1207@yonsei.ac.kr>
2021-08-18 21:49:29 +0900

    refactor: troubleshooting - 키보드가 올라왔을 때 메세지가 안보이는 문제 해결
    bottomConstraint를 하나 변수로 둔 뒤 해당 값에 messageInputStackView의 bottomAnchor의 같은 제약 부여
    키보드가 화면에 올라올 때마다 bottomConstraint의 constant값을 키보드 frame에 맞게 바뀌도록 구현하여
    위에 명시한 문제 해결

--- StreamChat/StreamChat/Controllers/ChatRoomViewController.swift
+++ StreamChat/StreamChat/Controllers/ChatRoomViewController.swift
@@ -14,6 +14,9 @@ class ChatRoomViewController: UIViewController {
     var myUserName = ""
     private var chatList: [Message] = []
     private let chatRoom = ChatRoom(chatNetworkManager: ChatNetworkManager())
+    private var bottomConstraint: NSLayoutConstraint?
+    
+    // MARK: - Views
     
     private let chatMessageView: UITableView = {
         let tableView = UITableView()
@@ -21,7 +24,7 @@ class ChatRoomViewController: UIViewController {
         tableView.translatesAutoresizingMaskIntoConstraints = false
         tableView.register(OthersMessageViewCell.self, forCellReuseIdentifier: OthersMessageViewCell.identifier)
         tableView.register(MyMessageViewCell.self, forCellReuseIdentifier: MyMessageViewCell.identifier)
-        tableView.keyboardDismissMode = .onDrag
+        tableView.keyboardDismissMode = .interactive
         tableView.separatorStyle = .none
         tableView.allowsSelection = false
         return tableView
@@ -30,7 +33,7 @@ class ChatRoomViewController: UIViewController {
     private let messageInputView: UIView = {
         let view = UIView()
         view.translatesAutoresizingMaskIntoConstraints = false
-        view.backgroundColor = .gray
+        view.backgroundColor = .systemGray6
         return view
     }()
     
@@ -39,22 +42,30 @@ class ChatRoomViewController: UIViewController {
         textField.translatesAutoresizingMaskIntoConstraints = false
         textField.setContentHuggingPriority(.defaultLow, for: .horizontal)
         textField.setContentCompressionResistancePriority(.defaultLow, for: .horizontal)
-        textField.backgroundColor = .white
+        textField.backgroundColor = .systemGray5
+        textField.autocorrectionType = .no
         return textField
     }()
     
     private let sendButton: UIButton = {
         let button = UIButton()
         button.translatesAutoresizingMaskIntoConstraints = false
-        button.backgroundColor = .brown
+        button.backgroundColor = .systemGreen
         button.titleLabel?.font = UIFont.preferredFont(forTextStyle: .body)
-        button.setTitle("전송", for: .normal)
-        button.layer.borderWidth = 1
-        button.layer.cornerRadius = 3
+        button.setTitle("send", for: .normal)
         button.setContentHuggingPriority(.defaultHigh, for: .horizontal)
         button.setContentCompressionResistancePriority(.defaultHigh, for: .horizontal)
         button.addTarget(self, action: #selector(sendMessage), for: .touchUpInside)
         return button
+    }()
+    
+    private lazy var messageInputStackView: UIStackView = {
+       let stackView = UIStackView(arrangedSubviews: [ messageInputTextField, sendButton])
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .horizontal
+        stackView.distribution = .fill
+        stackView.spacing = 5
+        return stackView
     }()
     
     // MARK: - Methods
@@ -84,24 +95,30 @@ class ChatRoomViewController: UIViewController {
     }
     
     @objc private func setViewLayoutWhenKeyboardShows(_ notification: Notification) {
+        
         guard let userInfo = notification.userInfo else { return }
         guard let keyboardFrame = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect else { return }
         
-        if self.view.bounds.origin.y == 0 {
-            self.view.bounds.origin.y += keyboardFrame.height
-        }
+        bottomConstraint?.constant = -keyboardFrame.height - 8
         
         guard let duration = userInfo[UIResponder.keyboardAnimationDurationUserInfoKey] as? TimeInterval else { return }
         
         UIView.animate(withDuration: duration) {
             self.view.layoutIfNeeded()
+            self.scrollToLastChat()
         }
     }
     
     @objc private func setViewLayoutWhenKeyboardHides(_ notification: Notification) {
-        if self.view.bounds.origin.y != 0 {
-            self.view.bounds.origin.y = 0
+        
+        guard let userInfo = notification.userInfo else { return }
+        guard let duration = userInfo[UIResponder.keyboardAnimationDurationUserInfoKey] as? TimeInterval else { return }
+        
+        bottomConstraint?.constant = -8
+        UIView.animate(withDuration: duration) {
+            self.view.layoutIfNeeded()
         }
+        
     }
     
     @objc private func sendMessage(_ sender: UIButton) {
@@ -114,13 +131,22 @@ class ChatRoomViewController: UIViewController {
         let indexPath = IndexPath(row: chatList.count - 1, section: 0)
         
         chatMessageView.insertRows(at: [indexPath], with: .none)
-        chatMessageView.scrollToRow(at: indexPath, at: .middle, animated: true)
+        chatMessageView.scrollToRow(at: indexPath, at: .bottom, animated: true)
+    }
+    
+    private func scrollToLastChat() {
+        guard !chatList.isEmpty else { return }
+        
+        let lastIndex = IndexPath(row: chatList.count - 1, section: 0)
+        
+        chatMessageView.scrollToRow(at: lastIndex, at: .bottom, animated: true)
+        
     }
     
     private func setUpChatRoomView() {
+        navigationItem.title = "개울챗"
         self.view.addSubview(messageInputView)
-        messageInputView.addSubview(messageInputTextField)
-        messageInputView.addSubview(sendButton)
+        messageInputView.addSubview(messageInputStackView)
         self.view.addSubview(chatMessageView)
         
         NSLayoutConstraint.activate([
@@ -128,20 +154,20 @@ class ChatRoomViewController: UIViewController {
             messageInputView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
             messageInputView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
             
-            messageInputTextField.topAnchor.constraint(equalTo: messageInputView.topAnchor, constant: 5),
-            messageInputTextField.leadingAnchor.constraint(equalTo: messageInputView.leadingAnchor, constant: 8),
-            messageInputTextField.trailingAnchor.constraint(equalTo: sendButton.leadingAnchor, constant: -5),
-            messageInputTextField.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor, constant: -8),
+            messageInputStackView.topAnchor.constraint(equalTo: messageInputView.topAnchor, constant: 5),
+            messageInputStackView.leadingAnchor.constraint(equalTo: messageInputView.leadingAnchor, constant: 5),
+            messageInputStackView.trailingAnchor.constraint(equalTo: messageInputView.trailingAnchor, constant: -5)
             
-            sendButton.topAnchor.constraint(equalTo: messageInputTextField.topAnchor),
-            sendButton.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -8),
-            sendButton.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor, constant: -8),
-            
+        ])
+        bottomConstraint =
+            messageInputStackView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor, constant: -8)
+        bottomConstraint?.isActive = true
+        
+        NSLayoutConstraint.activate([
             chatMessageView.topAnchor.constraint(equalTo: self.view.topAnchor),
             chatMessageView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
             chatMessageView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
-            chatMessageView.bottomAnchor.constraint(equalTo: self.messageInputView.topAnchor)
-            
+            chatMessageView.bottomAnchor.constraint(equalTo: self.messageInputView.topAnchor, constant: 10)
         ])
     }
 }
@@ -160,10 +186,6 @@ extension ChatRoomViewController: ChatReadable {
         }
     }
     
-    private func distinguishNotificationFromMessage(messageArray: [String]) {
-
-    }
-    
     private func receiveMessage(username: String, content: String) {
         guard username.isEmpty == false,
               content.isEmpty == false else { return }
@@ -173,7 +195,7 @@ extension ChatRoomViewController: ChatReadable {
         let indexPath = IndexPath(row: chatList.count - 1, section: 0)
         
         chatMessageView.insertRows(at: [indexPath], with: .none)
-        chatMessageView.scrollToRow(at: indexPath, at: .middle, animated: true)
+        chatMessageView.scrollToRow(at: indexPath, at: .bottom, animated: true)
     }
     
     private func receiveUserInformationMessage(content: String) {
@@ -184,7 +206,7 @@ extension ChatRoomViewController: ChatReadable {
         let indexPath = IndexPath(row: chatList.count - 1, section: 0)
         
         chatMessageView.insertRows(at: [indexPath], with: .none)
-        chatMessageView.scrollToRow(at: indexPath, at: .middle, animated: true)
+        chatMessageView.scrollToRow(at: indexPath, at: .bottom, animated: true)
     }
 }
 
