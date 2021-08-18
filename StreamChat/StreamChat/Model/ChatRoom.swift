61f5c7124aaebfc7fb36972fef717c6e0aa037ee
Inwoo Hwang <dlsdn1207@yonsei.ac.kr>
2021-08-18 14:56:18 +0900

    refactor: read() 메서드 분리 및 receiveChat() 메서드 추가
    기존 setUpNetwork() 에 포함 되어있던 read메서드를 분리한 뒤
    chatRoom의 receiveChat() 메서드에서 read메서드 호출

--- StreamChat/StreamChat/Model/ChatRoom.swift
+++ StreamChat/StreamChat/Model/ChatRoom.swift
@@ -20,27 +20,30 @@ final class ChatRoom: NSObject {
         chatNetworkManager.setUpNetwork()
     }
     
-    func joinChat(username: String) {
-        guard let data = "USR_NAME::\(username)::END".data(using: .utf8) else {
-            
-            return
+    func receiveChat() {
+        chatNetworkManager.read { [weak self] result in
+            switch result {
+            case .success(let data):
+                NSLog(String(data: data, encoding: .utf8)!)
+            case .failure(let error):
+                NSLog(error.localizedDescription)
+            }
         }
+    }
+    
+    func joinChat(username: String) {
+        guard let data = "USR_NAME::\(username)::END".data(using: .utf8) else { return }
+        self.receiveChat()
         chatNetworkManager.write(data: data)
     }
     
     func send(_ message: String) {
-        guard let data = "MSG::\(message)::END".data(using: .utf8) else {
-            
-            return
-        }
+        guard let data = "MSG::\(message)::END".data(using: .utf8) else { return }
         chatNetworkManager.write(data: data)
     }
     
     func leaveChat() {
-        guard let data = "LEAVE::::END".data(using: .utf8) else {
-            
-            return
-        }
+        guard let data = "LEAVE::::END".data(using: .utf8) else { return }
         chatNetworkManager.write(data: data)
         chatNetworkManager.closeStream()
     }
